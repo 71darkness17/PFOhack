56d46a40cc29acd8ce41cf4b64b156e67dbb6337
Владимир Заворохин <75090682+71darkness17@users.noreply.github.com>
2024-10-26 12:26:24 +0300

    Add files via upload

--- CreateDataset.py
+++ CreateDataset.py
@@ -28,27 +28,55 @@ def read_pdf(file_path):
 #     doc = Document(file_path)
 #     text = "\n".join([p.text for p in doc.paragraphs])
 #     return pd.DataFrame([line.split() for line in text.splitlines()])  # Уточните структуру
-def marketing_list(id: int,path: str) -> (list,list):
-    df = pd.read_excel(path).drop(columns=['Находится в реестре МСП', 'ОКВЭД2.Наименование', 'ОКВЭД2.Код','Город фактический',
-                                       'Город юридический','Грузоотправитель','Грузополучатель',
-                                       'Карточка клиента (внешний источник).Индекс платежной дисциплины Описание',
-                                       'Карточка клиента (внешний источник).Индекс финансового риска Описание',
-                                       'Госконтракты.Контракт','Госконтракты.Тип контракта'], errors='ignore')
+def change_the_data(typev: int,value):
+    if type(value) == str:
+        value = value.replace(' ', '')
+    match value:
+        case 'Микробизнес':
+            return 0
+        case 'Малыйбизнес':
+            return 1
+        case 'Среднийбизнес':
+            return 2
+        case 'Крупныйбизнес':
+            return 3
+        case None:
+            return 0
+        case '':
+            return 0
+        case _:
+            if typev == 4:
+                return 1
+                # except TypeError:
+                #     value = value.replace(' ', '#')
+                #     assert 1==2, f'len: {len(value)} type: {typev} value: {value}'
+            else:
+                return value
+
+def marketing_list(id: int,df: DataFrame) -> (list,list):
+    # df = pd.read_excel(path).drop(columns=['Находится в реестре МСП', 'ОКВЭД2.Наименование', 'ОКВЭД2.Код','Город фактический',
+    #                                    'Город юридический','Грузоотправитель','Грузополучатель',
+    #                                    'Карточка клиента (внешний источник).Индекс платежной дисциплины Описание',
+    #                                    'Карточка клиента (внешний источник).Индекс финансового риска Описание',
+    #                                    'Госконтракты.Контракт','Госконтракты.Тип контракта'], errors='ignore').fillna(0)
     res = []
-    rows = [df.iloc[i] for i in range(len(df['ID']))]
+    rows = [df.iloc[i].to_list() for i in range(len(df['ID']))]
     for row in rows:
-        if row.iloc[0] == id:
-            for i in range(1, len(row)):
-                res = row
-                break
-    res_col = df.columns
-    ans = []
-    for i in range(len(res)):
-        ans.append(res.iloc[i])
-    return ans, list(df.columns.values)
-def transition_value(id: int,path: str) -> (list,list):
-    df = pd.read_excel(path, skiprows=2).drop(columns=['Субъект федерации отп','Субъект федерации наз',
-                                                       'Код груза','Гр груза по опер.номен'], errors='ignore')
+        if row[0] == id:
+            for i in range(len(row)):
+                row[i] = change_the_data(i,row[i])
+            res = row
+
+            #print(res)
+            break
+    if len(res) > 0:
+        cols = ['ID', 'company_size', 'capital_size', 'emp_amount', 'is_els', 'payment_index','risk_index']
+    else:
+        cols = []
+    return res,cols
+def transition_value(id: int,df: DataFrame) -> (list,list):
+    # df = pd.read_excel(path, skiprows=2).drop(columns=['Субъект федерации отп','Субъект федерации наз',
+    #                                                    'Код груза','Гр груза по опер.номен'], errors='ignore')
     rows = [df.iloc[i] for i in range(len(df['ID']))]
     for row in rows:
         break
@@ -56,19 +84,19 @@ def transition_value(id: int,path: str) -> (list,list):
         #     for i in range(1,len(row)):
         #         pass
     return [],[]
-def requests(id: int, path: str) -> (list,list):
-    df = pd.read_excel(path).drop(columns=['Дата','Тема','Номер','Тип обращения','Группа вопросов','Количество доработок'], errors='ignore')
-    rows = [df.iloc[i] for i in range(len(df['ID']))]
+def requests(id: int, df: DataFrame) -> (list,list):
+    # df = pd.read_excel(path).drop(columns=['Дата','Тема','Номер','Тип обращения','Группа вопросов','Количество доработок'], errors='ignore')
+    rows = [df.iloc[i].to_list() for i in range(len(df['ID']))]
     reports = 0
     for row in rows:
-        if row.iloc[0] == id and row.iloc[1] == 'Жалобы':
+        if row[0] == id and row[1] == 'Жалобы':
             reports+=1
     l = [reports]
     return l, ['reports_amount']
-def interests(id: int, path: str):
-    df = pd.read_excel(path).drop(columns=['Дата','Тема','Сценарий','Подразделение','Ожидаемая выручка','Вероятность сделки, %','Дата следующей активности',
-                                          'Следующая активность','Канал первичного интереса','Номер',
-                                           'Ссылка (служебное поле для вывода на экран прочих реквизитов объекта)'], errors='ignore')
+def interests(id: int, df: DataFrame):
+    # df = pd.read_excel(path).drop(columns=['Дата','Тема','Сценарий','Подразделение','Ожидаемая выручка','Вероятность сделки, %','Дата следующей активности',
+    #                                       'Следующая активность','Канал первичного интереса','Номер',
+    #                                        'Ссылка (служебное поле для вывода на экран прочих реквизитов объекта)'], errors='ignore')
     res_dict = {
         'Завершен неудачно': 0,
         'Завершен успешно': 0,
@@ -81,44 +109,96 @@ def interests(id: int, path: str):
         if row.iloc[1] == id:
             if row.iloc[0] in res_dict.keys():
                 res_dict[row.iloc[0]]+=1
-    return res_dict.values(), ['fail','success','registration','denied','once_offer']
-def create_dataset(paths: list) -> pd.DataFrame:
+    return list(res_dict.values()), ['fail','success','registration','denied','once_offer']
+def create_dataset(paths: list, output: str):
     id_table = pd.read_excel(paths[-1])
+    tables = list()
+    for i in range(8):
+        tables.append(pd.read_excel(paths[i]).drop(columns=['Находится в реестре МСП', 'ОКВЭД2.Наименование', 'ОКВЭД2.Код','Город фактический',
+                                       'Город юридический','Грузоотправитель','Грузополучатель',
+                                       'Карточка клиента (внешний источник).Индекс платежной дисциплины Описание',
+                                       'Карточка клиента (внешний источник).Индекс финансового риска Описание',
+                                       'Госконтракты.Контракт','Госконтракты.Тип контракта'], errors='ignore').fillna(0))
+    tables.append(pd.read_excel(paths[8]).drop(columns=['Дата','Тема','Сценарий','Подразделение','Ожидаемая выручка','Вероятность сделки, %','Дата следующей активности',
+                                          'Следующая активность','Канал первичного интереса','Номер',
+                                           'Ссылка (служебное поле для вывода на экран прочих реквизитов объекта)'], errors='ignore'))
+    tables.append(pd.read_excel(paths[9]).drop(columns=['Дата','Тема','Номер','Тип обращения','Группа вопросов','Количество доработок'], errors='ignore'))
+    tables.append(pd.read_excel(paths[10], skiprows=2).drop(columns=['Субъект федерации отп','Субъект федерации наз',
+                                                       'Код груза','Гр груза по опер.номен'], errors='ignore'))
     ids = [i for i in id_table['ID']]
     predataset = list()
     columns1 = list()
     columns = list()
-    j = 0
-    for id in ids[847:848]:
-        temp = []
-        g = 8
-        for i in range(len(paths)-1):
-            if i<g:
-                temp1, ctemp = marketing_list(id,paths[i])
-                temp+=temp1
-                if len(temp1)>0:
-                    columns1+=ctemp
-                    g = 0
-            elif i == 8:
-                temp1, ctemp = interests(id,paths[i])
-                temp += temp1
-                columns1 += ctemp
-            elif i == 9:
-                temp1, ctemp = requests(id,paths[i])
-                temp += temp1
-                columns1 += ctemp
-            elif i == 10:
-                temp1, ctemp = transition_value(id,paths[i])
-                temp += temp1
-                columns1 += ctemp
-        predataset.append(temp)
-        if j == 0:
-            columns=columns1
-            j+=1
+    j = True
+    end = False
 
+    # for i in range(len(paths) - 1):
+    #     id = len(ids)/2+1
+    #     if i == 0:
+    #         temp1, ctemp = marketing_list(id, paths[i])
+    #         # if i == 0:
+    #         #     temp += temp1
+    #         # else:
+    #         #     temp = temp1
+    #         columns1+=ctemp
+    #         # if temp[1] != 0:
+    #         #     end = True
+    #     elif i == 8:
+    #         # temp1, ctemp = interests(id, paths[i])
+    #         # temp += temp1
+    #         columns1 += ctemp
+    #     elif i == 9:
+    #         # temp1, ctemp = requests(id, paths[i])
+    #         # temp += temp1
+    #         columns1 += ctemp
+    #     elif i == 10:
+    #         columns1 += ctemp
+    # columns = columns1
+    for id in ids[847:850]:
+        temp = []
+        if len(columns)>0:
+            j = False
+        end = False
+        for i in range(len(tables)):
+            if i<8 and not end:
+                temp1, ctemp = marketing_list(id,tables[i])
+
+                if i == 0:
+                    temp+=temp1
+                    print(temp1)
+                elif len(temp1)>0:
+                    temp = temp1
+                    print(temp1)
+                if not 'ID' in columns1:
+                    columns1+=ctemp
+                if len(temp)>0 and temp[1] != 0:
+                    end = True
+
+            elif i == 8:
+                temp1, ctemp = interests(id,tables[i])
+                temp += temp1
+                if not ctemp[0] in columns1:
+                    columns1 += ctemp
+            elif i == 9:
+                temp1, ctemp = requests(id,tables[i])
+                temp += temp1
+                if not ctemp[0] in columns1:
+                    columns1 += ctemp
+            # elif i == 10:
+            #     temp1, ctemp = transition_value(id,tables[i])
+            #     temp += temp1
+            #     if not ctemp[0] in columns1:
+            #         columns1 += ctemp
+        predataset.append(temp)
+        columns=columns1
+
+    for data in range(len(predataset)):
+        assert len(predataset[data]) == len(columns), f'id: {data} | {len(predataset[data]),predataset[data], len(columns),columns}'
     try:
         dataset = pd.DataFrame(data=predataset, columns=columns)
     except ValueError:
         print(len(columns), len(predataset[0]))
-        dataset = []
-    return dataset
+        print(columns)
+        print(predataset)
+        dataset = pd.DataFrame(data=[], columns=[])
+    dataset.to_csv(output)
